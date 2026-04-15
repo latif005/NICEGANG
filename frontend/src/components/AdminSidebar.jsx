a5222feb186d69ae07e4f4bf7cde7f8f01a8c638
angel <angelmexgap>
2026-04-15 15:34:04 +0700

    membuat sidebar admin

---
+++ frontend/src/components/AdminSidebar.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+function AdminSidebar() {
+    return (
+        <div style={{
+            width: "220px",
+            background: "#111",
+            color: "white",
+            minHeight: "100vh",
+            padding: "20px"
+        }}>
+            <h2>Admin Panel</h2>
+
+            <Link to="/admin">Dashboard</Link><br />
+            <Link to="/admin/games">Games</Link><br />
+            <Link to="/admin/packages">Packages</Link><br />
+            <Link to="/admin/promos">Promos</Link><br />
+        </div>
+    );
+}
+
+export default AdminSidebar;
