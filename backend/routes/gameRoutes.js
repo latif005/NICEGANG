dd231b245d75f040041ed0bcd385bbc7ae786e38
adinda <adindaac12@gmail.com>
2026-04-25 22:57:19 +0700

    add middleware

--- backend/routes/gameRoutes.js
+++ backend/routes/gameRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const gameController = require("../controllers/gameController");
+const { verifyToken } = require("../middleware/authMiddleware");
+const roleMiddleware = require("../middleware/roleMiddleware");
 
 router.get("/games", gameController.getGames);
-router.post("/games", gameController.createGame);
-router.put("/games/:id", gameController.updateGame);
-router.delete("/games/:id", gameController.deleteGame);
-
+router.post("/games", verifyToken, roleMiddleware, gameController.createGame);
+router.put("/games/:id", verifyToken, roleMiddleware, gameController.updateGame);
+router.delete("/games/:id", verifyToken, roleMiddleware, gameController.deleteGame);
 module.exports = router;
